17604d0f4df290e70061a0323e46660fe981bcc8
Romulo Soares <romulosoares@ice.ufjf.br>
2020-11-20 14:14:27 -0300

    Configurei o form.js para chamar a funçao para verificar se existe uma rede

--- routes/form.js
+++ routes/form.js
@@ -3,19 +3,23 @@ const router = express.Router();
 const createNetwork = require('../app/controller/createNetwork')
 const mongodb = require('../app/database/mongodb/Server')
 const postRede = require('../app/database/rede/input/postRede')
+const getRede = require('../app/database/rede/output/getRede')
 
 
 //Insere os dados na database e Network
 router.post('/sendData', async (req, res) => {
-
-    if (req.body != null) {
-        createNetwork.creatAndSave(req, res);
-        postRede.postRedeDatabase(req, res)
-
-
-        res.redirect('/result?msg=success');
+    const verify = await createNetwork.verifyNetworkName(req,res);
+    if (String(verify) === "exist") {
+        res.redirect('/result?msg=existFile');
     } else {
-        res.redirect('/result?msg=failed');
+        if (req.body != null) {
+            createNetwork.creatAndSave(req, res);
+            postRede.postRedeDatabase(req, res);
+
+            res.redirect('/result?msg=success');
+        } else {
+            res.redirect('/result?msg=failed');
+        }
     }
 });
 
