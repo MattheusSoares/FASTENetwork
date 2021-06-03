dfca466d46c8b2b6b258fa6b2cfbda62904d3596
romulolassoares <romulosoares@ice.ufjf.br>
2021-06-03 18:34:12 -0300

    Novos arquivos

---
+++ template/createOrgs/createOrgs.sh
@@ -0,0 +1,11 @@
+function createOrgs() {
+   # Remove as pastas se existirem
+   if [ -d "organizations/peerOrganizations" ]; then
+      rm -Rf organizations/peerOrganizations && rm -Rf organizations/ordererOrganizations
+   fi
+
+   which cryptogen
+   if [ "$?" -ne 0 ]; then
+      fatalln "cryptogen tool not found. exiting"
+   fi
+   infoln "Generating certificates using cryptogen tool"
